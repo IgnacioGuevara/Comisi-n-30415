280103d6f6095a2479c5ceeb5e6f85cffac0e1ba
Ignacio Guevara <ignacioguevaragilardoni@gmail.com>
2022-06-22 03:04:05 -0300

    1er maquetacion de la pagina

--- pages/archivo.js
+++ pages/archivo.js
@@ -2,6 +2,82 @@ function nombreCompleto() {
     let nombre = prompt ("Ingrese su nombre completo.");
         alert ("Bienvenido " + nombre + " !!");
 }
+function generoRopa() {
+    let genero = prompt("¿Qué genero de prenda está buscando?");
+        switch (genero) {
+            case  "masculino":
+                alert("masculino")
+                break;
+            case 2 || "femenino":
+                alert("femenino")
+                break;
+            case 3 || "niño/a":
+                alert("niño")
+                break;
+            case 4 || "unisex":
+                alert("unisex")
+                break;
+        
+            default:
+                alert("sada")
+                break;
+        }
+    }
+
+
+function tipoPrenda() {
+    let prenda = prompt("¿Qué tipo de prenda esta buscando? ");
+        switch (prenda) {
+            case 1 || "pantalon":
+                alert("pantalon")
+                break;
+            case 2 || "remera":
+                alert("remera")
+                break;
+            case 3 || "medias":
+                alert("media")
+                break;
+            case 4 || "buzo" || "campera":
+                alert("buzo")
+                break;
+        
+            default:
+                break;
+        }
+        
+    }
+
+
+function talle() {
+    let talle = prompt ("¿Qué talle está buscando?")
+        switch (talle) {
+            case 1 || "s" :
+                alert("s")
+                break;
+            case 2 || "m" :
+                alert("m")
+                break;
+            case 3 || "l":
+                alert("l")
+                break;
+            case 4 || "xl" :
+                alert("xl")
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+/*     nombreCompleto(); */
+    generoRopa();
+    tipoPrenda();
+    talle();
+
+
+/* 
+
+
 
 
 function carnetConducir() {
@@ -50,4 +126,4 @@ function edad() {
 
 nombreCompleto();
 edad();
-
+ */
