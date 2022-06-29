e9b5095ecb40443b72a3f840ae5d78e1b08239cf
Ignacio Guevara <ignacioguevaragilardoni@gmail.com>
2022-06-29 18:31:00 -0300

    Desafio complementario 2

--- pages/archivo.js
+++ pages/archivo.js
@@ -22,7 +22,7 @@ function precio() {
 function edadYgenero() {
     let años = parseFloat(prompt("Ingrese su edad"));
     if (años >= 13) {
-        registroGenero();
+        Generos();
     
     } else if (años < 13) {
         alert("Debe ser mayor de 13 años para poder ingresar al sitio web.");
@@ -33,13 +33,13 @@ function edadYgenero() {
 }
 /* ----GENERO---- */
 function registroGenero() {
-    let Registro = prompt("¿Que genero esta buscando? \n - gratuitos \n - nuevos lanzamientos \n - deportivos \n - accion \n - proximamentes ");
+    let Registro = prompt("¿Que genero esta buscando? \n - gratis \n - nuevo lanzamiento \n - deporte \n - accion \n - proximamente ");
     switch(Registro){
         case "gratis":
             alert("ha ingresado a la seccion de: Juegos Gratuitos. ");
             precio();
         break;
-        case "Nuevos lanzamientos":
+        case "Nuevo lanzamiento":
             alert("ha ingresado a la seccion de: Nuevos lanzamientos. ");
             precio();
         break;
@@ -61,6 +61,43 @@ function registroGenero() {
     }
 }
 
-
 nombreCompleto();
-edadYgenero();
+edadYgenero(); 
+
+
+
+function Generos() {
+
+const videoJuegos = [
+    { nombre: "amoung Us", precio: 0  , categoria: "gratis" },
+    { nombre: "fall Guys", precio: 0, categoria: "gratis" },
+    { nombre: "pool", precio: 0, categoria: "gratis" },
+    
+    { nombre: "fifa 22", precio: 90, categoria: "deportes" },
+    { nombre: "pes 22", precio: 50, categoria: "deportes" },
+    { nombre: "pes 2K22", precio: 70, categoria: "deportes" },
+    { nombre: "wwe 22", precio: 30, categoria: "deportes" },
+    { nombre: "nhl 2K22", precio: 70, categoria: "deportes" },
+    
+    { nombre: "uncharted 4", precio: 89, categoria: "nuevo lanzamiento" },
+    { nombre: "tomb raider", precio: 56, categoria: "nuevo lanzamiento" },
+    
+    { nombre: "Call of duty", precio: 98, categoria: "accion" },
+    { nombre: "counter strike", precio: 25, categoria: "accion" },
+    { nombre: "battlefield 2042", precio: 93, categoria: "accion" },
+    { nombre: "god of war", precio: 8, categoria: "accion" },
+
+    { nombre: "league of legends", precio: 8, categoria: "proximamente" },
+
+    
+];
+
+let categoria = prompt("ingresa la categoria \n - gratis \n - nuevo lanzamiento \n - deporte \n - accion \n - proximamente ");
+
+let filtradoCategoria = videoJuegos.filter(elemento => elemento.categoria === categoria);
+
+let nombre = prompt("Ingrese el nombre del juego");
+let filtradoNombre = filtradoCategoria.filter(elemento => elemento.nombre.includes(nombre));
+console.log(filtradoNombre);
+}
+
