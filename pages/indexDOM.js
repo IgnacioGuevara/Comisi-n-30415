c92e35e8971dd432dd969207c06be3c590df0277
Ignacio Guevara <ignacioguevaragilardoni@gmail.com>
2022-07-25 21:11:38 -0300

    Agregando libreria

--- pages/indexDOM.js
+++ pages/indexDOM.js
@@ -5,26 +5,24 @@
 
 
 let videoJuegos = [
-        { nombre: "amoung Us", precio: 0 , categoria: "1", img: "./img/amoungUs.webp"  },
-        { nombre: "fall Guys", precio: 0 , categoria: "1", img: "./img/fallGuys.jpg" },
-        { nombre: "pool", precio: 0 , categoria: "1", img: "./img/pool.jpg" },
-        
-        { nombre: "fifa 22", precio: 50 , categoria: "2", img: "./img/fifa22.avif" },
-        { nombre: "pes 2021", precio: 70 , categoria: "2", img: "./img/pes2021.jpg" },
-        { nombre: "wwe 22", precio: 3 , categoria: "2", img: "./img/wwe22.jpg" },
-        { nombre: "nhl 2K22", precio: 70 , categoria: "2", img: "./img/nhl2022.jpg" },
-        
-        { nombre: "uncharted 4", precio: 89 , categoria: "3", img: "./img/uncharted.jpg" },
-        { nombre: "tomb raider", precio: 56 , categoria: "3", img: "./img/tombraider.jpg" },
-        
-        { nombre: "call of duty", precio: 98 , categoria: "4", img: "./img/callofduty.jpg" },
-        { nombre: "counter strike", precio: 25 , categoria: "4", img: "./img/counterstrike.jpg" },
-        { nombre: "battlefield 2042", precio: 93 , categoria: "4", img: "./img/battlefield.jpg" },
-        { nombre: "god of war", precio: 8 , categoria: "4", img: "./img/godOfWar.jpg" },
+    { nombre: "amoung Us", precio: 0 , categoria: "1", img: "./img/amoungUs.webp"  },
+    { nombre: "fall Guys", precio: 0 , categoria: "1", img: "./img/fallGuys.jpg" },
+    { nombre: "pool", precio: 0 , categoria: "1", img: "./img/pool.jpg" },
     
-        { nombre: "league of legends", precio: 8 , categoria: "5", img: "./img/LOL.jpg" },
+    { nombre: "fifa 22", precio: 50 , categoria: "2", img: "./img/fifa22.avif" },
+    { nombre: "pes 2021", precio: 70 , categoria: "2", img: "./img/pes2021.jpg" },
+    { nombre: "wwe 22", precio: 3 , categoria: "2", img: "./img/wwe22.jpg" },
+    { nombre: "nhl 2K22", precio: 70 , categoria: "2", img: "./img/nhl2022.jpg" },
     
-        
+    { nombre: "uncharted 4", precio: 89 , categoria: "3", img: "./img/uncharted.jpg" },
+    { nombre: "tomb raider", precio: 56 , categoria: "3", img: "./img/tombraider.jpg" },
+    
+    { nombre: "call of duty", precio: 98 , categoria: "4", img: "./img/callofduty.jpg" },
+    { nombre: "counter strike", precio: 25 , categoria: "4", img: "./img/counterstrike.jpg" },
+    { nombre: "battlefield 2042", precio: 93 , categoria: "4", img: "./img/battlefield.jpg" },
+    { nombre: "god of war", precio: 8 , categoria: "4", img: "./img/godOfWar.jpg" },
+
+    { nombre: "league of legends", precio: 8 , categoria: "5", img: "./img/LOL.jpg" },
 ];
 
 function crearTarjeta() {
@@ -72,33 +70,64 @@ crearTarjeta();
 
     let boton1 = document.getElementById("boton1")
     boton1.addEventListener("click", ascendente)
-    function ascendente() {
-        let ordenadosAZ = [...videoJuegos].sort((a, b) =>{
-            if (a.nombre > b.nombre) {
-                return 1
-            } else if (a.nombre < b.nombre){
-                return -1
-            } else{
-                return 0
-            }
-        })
-        cargarPoductos(ordenadosAZ)
-    }
+
+function ascendente() {
+    let ordenadosAZ = [...videoJuegos].sort((a, b) =>{
+        if (a.nombre > b.nombre) {
+            return 1
+        } else if (a.nombre < b.nombre){
+            return -1
+        } else{
+            return 0
+        }
+    })
+    cargarPoductos(ordenadosAZ)
+    Swal.fire({
+        title: "ORDENADO: A-Z",
+        padding:"1.rem",
+        background:"#CFF69F",
+        backdrop:false,
+        timer:"3000",
+        timerProgressBar:true,
+        toast: true,
+        position:"bottom-end",
+        showConfirmButton: false,
+        showCloseButton:true,
+        customClass:{
+            popup: 'alert'
+        },
+    });
+}
 
     let boton2 = document.getElementById("boton2")
     boton2.addEventListener("click", descendente)
-    function descendente() {
-        let ordenadosZA = [...videoJuegos].sort((a, b) =>{
-            if (a.nombre > b.nombre) {
-                return -1
-            } else if (a.nombre < b.nombre){
-                return 1
-            } else{
-                return 0
-            }
-        })
-        cargarPoductos(ordenadosZA)
-    }
+function descendente() {
+    let ordenadosZA = [...videoJuegos].sort((a, b) =>{
+        if (a.nombre > b.nombre) {
+            return -1
+        } else if (a.nombre < b.nombre){
+            return 1
+        } else{
+            return 0
+        }
+    })
+    cargarPoductos(ordenadosZA)
+    Swal.fire({
+        title: "ORDENADO: Z-A",
+        padding:"1.rem",
+        background:"#CFF69F",
+        backdrop:false,
+        timer:"3000",
+        timerProgressBar:true,
+        toast: true,
+        position:"bottom-end",
+        showConfirmButton: false,
+        showCloseButton:true,
+        customClass:{
+            popup: 'alert'
+        },
+    });
+}
 
 
     verCarrito.addEventListener("click", () => {
---
+++ pages/sweetAlert.js
@@ -0,0 +1,31 @@
+/* Swal.fire({
+    title: "ORDENADO: A-Z",
+    padding:"1.rem",
+    background:"#CFF69F",
+    backdrop:false,
+    timer:"3000",
+    timerProgressBar:true,
+    toast: true,
+    position:"bottom-end",
+    showConfirmButton: false,
+    showCloseButton:true,
+    customClass:{
+        popup: 'alert'
+    },
+});
+
+Swal.fire({
+    title: "ORDENADO: Z-A",
+    padding:"1.rem",
+    background:"#CFF69F",
+    backdrop:false,
+    timer:"3000",
+    timerProgressBar:true,
+    toast: true,
+    position:"bottom-end",
+    showConfirmButton: false,
+    showCloseButton:true,
+    customClass:{
+        popup: 'alert'
+    },
+}); */
